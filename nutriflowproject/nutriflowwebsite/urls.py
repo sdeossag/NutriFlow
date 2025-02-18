1651a10c457e60c42d26d910159a62096b9ff809
sdeossag <sdeossag@eafit.edu.co>
2025-02-18 17:04:52 -0500

    Commit templates y signup

--- nutriflowproject/nutriflowwebsite/urls.py
+++ nutriflowproject/nutriflowwebsite/urls.py
@@ -24,6 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mealViews.home),
+    path('signup/', mealViews.signup),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

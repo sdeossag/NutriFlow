a71ba6a6290dd2909f5f53d192ecff58021ec0a2
sdeossag <sdeossag@eafit.edu.co>
2025-05-21 09:04:55 -0500

    Presupuesto y regenerar dieta

--- ia/urls.py
+++ ia/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('generate-diet/', views.generate_meal_plan, name='generate_diet'),
+     path('plan-generado/', views.mostrar_plan_generado, name='mostrar_plan_generado'),
 ]
--- meal/views.py
+++ meal/views.py
@@ -3,6 +3,7 @@ from preferences.models import Preferences
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
